9e0c31fdeb38c951fae612e9872cad545b2e295b
analopez03 <analopezdeahumada97@gmail.com>
2025-10-01 10:23:22 +0200

    comentario 2

--- saludo.js
+++ saludo.js
@@ -3,5 +3,5 @@ const apellido1 = "López";
 const apellido2 = "De Ahumada";
 
 console.log('Hola',nombre, apellido1, apellido2,'!');
-
+console.log('Hola',nombre, apellido1, apellido2,'!');
 console.log('Voy a aprobar el módulo de DWEC');
