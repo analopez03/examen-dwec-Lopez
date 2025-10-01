a3bf532a26bd31c4e6f7d623fedb7cf64d769171
analopez03 <analopezdeahumada97@gmail.com>
2025-10-01 09:56:53 +0200

    Comprobación para pull request

--- saludo.js
+++ saludo.js
@@ -3,6 +3,7 @@ const apellido1 = "López";
 const apellido2 = "De Ahumada";
 
 console.log('Hola',nombre, apellido1, apellido2,'!');
+console.log('Hola',nombre, apellido1, apellido2,'!');
 
 console.log('Voy a aprobar el módulo de DWEC');
 console.log('Voy a aprobar el módulo de DWEC');
